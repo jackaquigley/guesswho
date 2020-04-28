c3b46c985b4a5b787734ee9649cdb3cc9071e495
Jack Quigley <jackaquigley@gmail.com>
2020-04-28 13:55:35 +0100

    inital commit, added data from api to characters array

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data() {
+    return {
+      characters: []
+    }
+  },
   components: {
-    HelloWorld
+
+  },
+  mounted() {
+      fetch('https://codeclan-guess-who.herokuapp.com/api/characters')
+      .then(res => res.json())
+      .then(data => this.characters = data)
+
   }
 }
+
 </script>
 
 <style>
